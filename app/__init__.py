a670f00981200df25d8bd24e3042368d8d870793
Sebastian Blaeschke <sb@shv-ag.local>
2019-06-11 23:23:26 +0200

    2nd Initial Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,11 @@
 from flask import Flask
-from flask_mail import Mail
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from config import config
 
-moment = Moment()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
-    moment.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
